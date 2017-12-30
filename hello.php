0e5bcfc55f6b411a5724fa817d9a9dd7f8ca8c23
robicse <robicse8@gmail.com>
2017-12-30 10:14:05 +0600

    Added hello.php file

---
+++ hello.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 12/29/2017
+ * Time: 11:21 PM
+ */
+class hello
+{
+    function main()
+    {
+        echo "phpstrom code editor is good";
+    }
+
+}
